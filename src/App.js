3b746a2611d7704e4655332a4eeffc99ce8540ad
= <=markmella09@gmail.com>
2022-05-22 23:38:30 +0800

    edit pos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Pos from './Pos';
-import Bmi from './Bmi'; 
 
 export default function App() {
     return(
         <div>
-            {/* <Pos /> */}
-            {/* <Bmi /> */}
+            <Pos />
         </div>
     )
 }
